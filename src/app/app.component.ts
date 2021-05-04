256ead1a8a93f0003e4183e875c325297a457507
daianaiurescu <daiana.iurescu@gmail.com>
2021-05-04 17:06:41 +0300

    added some style to the home page and created the shop and cart pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {CartService} from './services/cartService';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +9,26 @@ import {Router} from '@angular/router';
 })
 export class AppComponent implements OnInit{
   title = 'Little-Helpers';
-  constructor(private router: Router) {}
+  cartNumber = 0;
+  constructor(private router: Router, private cartService: CartService) {
+      this.cartService.cartSubject.subscribe(data => this.cartNumber = data);
+  }
 
   ngOnInit(): void {
-    console.log('home');
+    this.cartNumberFunc();
   }
   goToLogInPage(): void{
     this.router.navigate(['login']);
+  }
+  goToCart(): void{
+    this.router.navigate(['cart']);
+  }
+  cartNumberFunc(): void {
+    if
+    (localStorage.getItem
+    ('localCart') != null) {
+      const cartCount = JSON.parse(localStorage.getItem('localCart'));
+      this.cartNumber = cartCount.length;
+    }
   }
 }
